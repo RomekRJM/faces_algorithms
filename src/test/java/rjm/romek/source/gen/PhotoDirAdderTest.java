37f84eb22a101233b007ec2a3545955b0b70475a
RomekRJM <rjm_20@o2.pl>
2014-11-17 23:04:07 +0000

    Issue-2 Rename photo directories, to contain country rather then denonyms.

--- src/test/java/rjm/romek/source/gen/PhotoDirAdderTest.java
+++ src/test/java/rjm/romek/source/gen/PhotoDirAdderTest.java
@@ -1,10 +1,8 @@
 package rjm.romek.source.gen;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -13,45 +11,35 @@ import rjm.romek.source.model.Country;
 
 public class PhotoDirAdderTest {
 	
-	PhotoDirAdder pda = new PhotoDirAdder();
-	
 	@Test
-	public void testAllCountriesMatchedToDir() {
+	public void testAllCountriesMatchedToDirName() {
 		Set<Country> countries = new CsvDeserializer().deserialize(new File(
 				"src/main/resources/list.csv"));
 		
-		pda.addPhotosOfFamousPeopleDir(countries, new File("src/main/resources/photos"));
+		List<String> allDirNames = prepareDirNamesList(new File("src/main/resources/photos"));
 		
 		for(Country country : countries) {
-			assertNotNull(country.getName() + " does not have matching folder!", country.getPhotoFolder());
-			System.out.println(country.getName() + " matches " + country.getPhotoFolder());
+			//assertNotNull(country.getName() + " does not have matching folder!", country.getPhotoFolder());
+			boolean removed = allDirNames.remove(country.getName());
+			System.out.println(country.getName());
+			
 		}
 	}
-
-	@Test
-	public void testFindLongestCommonSubstring() {
-		
-		assertEquals(1, pda.findLongestCommonSubstring("I need a dollar!", "I"));
-		assertEquals(4, pda.findLongestCommonSubstring("I need a dollar!", "need"));
-		assertEquals(3, pda.findLongestCommonSubstring("I need a dollar!", "ar!"));
-		assertEquals(0, pda.findLongestCommonSubstring("I need a dollar!", "xyz"));
-		assertEquals(0, pda.findLongestCommonSubstring("I need a dollar!", null));
-		assertEquals(0, pda.findLongestCommonSubstring("need", "I need a dollar!"));
-	}
 	
-	@Test
-	public void testFindClosestMatch() {
-		Collection<String> all = new ArrayList<String>();
+	private List<String> prepareDirNamesList(File nationalityDir) {
+		List<String> map = new ArrayList<String>();
 		
-		all.add("Algerians");
-		all.add("Americans");
-		all.add("Azeris");
-		all.add("Bosniaks");
+		if(!nationalityDir.isDirectory()) {
+			return map;
+		}
 		
-		String query = "Azerbejian";
+		File[] photoDirs = nationalityDir.listFiles();
 		
-		assertEquals("Azeris", pda.findClosestMatch(all, query));
+		for(File p : photoDirs) {
+			map.add(p.getName());
+		}
 		
+		return map;
 	}
 
 }
